6ac460e78d1a87a1753500dd6309b0928d9cdf7a
Mike Solomon <mike@meeshkan.com>
2020-10-08 21:36:13 +0300

    Adds more classes

--- src/Type/Data/Graph.purs
+++ src/Type/Data/Graph.purs
@@ -8,6 +8,7 @@ import Prim.TypeError (class Fail, Text)
 import Record.Extra (SCons, SLProxy, SNil, kind SList)
 import Type.Data.Boolean (class Not)
 import Type.Data.Peano (class CompareNat, class SumNat, Succ, Z, kind Nat)
+import Type.RowList (class ListToRow)
 
 infixr 4 type SCons as :/
 
@@ -35,6 +36,11 @@ class SListGate (b :: Boolean) (left :: SList) (right :: SList) (o :: SList) | b
 
 instance sListGateL :: SListGate True left right left
 else instance sListGateR :: SListGate False left right right
+
+class NatGate (b :: Boolean) (left :: Nat) (right :: Nat) (o :: Nat) | b left right -> o
+
+instance natGateL :: NatGate True left right left
+else instance natGateR :: NatGate False left right right
 
 class ReverseInternal (acc :: SList) (s :: SList) (r :: SList) | acc s -> r
 
@@ -182,6 +188,16 @@ class Connected (graph :: # Type)
 instance connected :: (IsConnected graph True) => Connected graph
 else instance connectedFail :: (Fail (Text "Graph is not connected"), IsConnected graph True) => Connected graph
 
+class AllNodes (graph :: # Type) (nodes :: SList) | graph -> nodes
+
+class AllNodesInternal (graph :: RowList) (nodes :: SList) | graph -> nodes
+
+instance allNodes :: (RowToList graph gl, AllNodesInternal gl nodes) => AllNodes graph nodes
+
+instance allNodesInternalNil :: AllNodesInternal Nil SNil
+
+instance allNodesInternalCons :: AllNodesInternal tail nodes => AllNodesInternal (Cons k v tail) (SCons k nodes)
+
 class AllEdgesInternal (acc :: SList) (graph :: RowList) (edges :: SList) | acc graph -> edges
 
 instance allEdgesInternalNil :: AllEdgesInternal acc Nil acc
@@ -195,6 +211,41 @@ instance allEdgesInternalCons ::
 class AllEdges (graph :: # Type) (edges :: SList) | graph -> edges
 
 instance allEdges :: (RowToList graph gl, AllEdgesInternal SNil gl o, RemoveDuplicates o edges) => AllEdges graph edges
+
+class IsTerminus (k :: Symbol) (graph :: # Type) (b :: Boolean) | k graph -> b
+
+instance isTerminus ::
+  ( RowToList graph gl, Lookup k gl r, Length r nedges, CompareNat nedges Z e, IsEq e b
+  ) =>
+  IsTerminus k graph b
+
+class NTerminiInternal (gl :: RowList) (graph :: # Type) (n :: Nat) | gl -> n
+
+instance nTerminiInternalNil :: NTerminiInternal Nil graph Z
+
+instance nTerminiInternalCons ::
+  ( IsTerminus h graph it
+  , NatGate it (Succ Z) Z toAdd
+  , NTerminiInternal t graph rest
+  , SumNat toAdd rest o
+  ) =>
+  NTerminiInternal (Cons h (SLProxy v) t) graph o
+
+class NTermini (graph :: # Type) (n :: Nat) | graph -> n
+
+instance nTermini :: (RowToList graph gl, NTerminiInternal gl graph n) => NTermini graph n
+
+class HasUniqueTerminus (graph :: # Type) (b :: Boolean) | graph -> b
+
+instance hasUniqueTerminus ::
+  ( NTermini graph n, CompareNat n (Succ Z) e, IsEq e b
+  ) =>
+  HasUniqueTerminus graph b
+
+class WithUniqueTerminus (graph :: # Type)
+
+instance withUniqueTerminus :: (HasUniqueTerminus graph True) => WithUniqueTerminus graph
+else instance withUniqueTerminusFail :: (Fail (Text "This graph either has no terminus or has multiple termini"), HasUniqueTerminus graph False) => WithUniqueTerminus graph
 
 class HasOrphanNodes (graph :: # Type) (b :: Boolean) | graph -> b
 
@@ -218,3 +269,87 @@ class NoOrphanedNodes (graph :: # Type)
 
 instance noOrphanedNodes :: (HasOrphanNodes graph False) => NoOrphanedNodes graph
 else instance noOrphanedNodesFail :: (Fail (Text "This graph has orphaned nodes"), HasOrphanNodes graph True) => NoOrphanedNodes graph
+
+class HasDuplicateNodes (graph :: # Type) (b :: Boolean) | graph -> b
+
+instance hasDuplicateNodes ::
+  ( AllNodes graph allNodes
+  , RemoveDuplicates allNodes allNodesx
+  , Length allNodes lAllNodes
+  , Length allNodesx lAllNodesx
+  , CompareNat lAllNodes lAllNodesx e
+  , IsEq e bNot
+  , Not bNot b
+  ) =>
+  HasDuplicateNodes graph b
+
+class NoDuplicateNodes (graph :: # Type)
+
+instance noDuplicateNodes :: (HasDuplicateNodes graph False) => NoDuplicateNodes graph
+else instance noDuplicateNodesFail :: (Fail (Text "This graph has duplicate nodes"), HasDuplicateNodes graph True) => NoDuplicateNodes graph
+
+instance hasDuplicateEdgesInternalTrue :: HasDuplicateEdgesInternal True graph True
+else instance hasDuplicateEdgesInternalNil :: HasDuplicateEdgesInternal v Nil v
+else instance hasDuplicateEdgesInternal ::
+  ( RemoveDuplicates v d
+  , Length v lv
+  , Length d ld
+  , CompareNat lv ld e
+  , IsEq e bNot
+  , Not bNot toLoop
+  , HasDuplicateEdgesInternal toLoop t b
+  ) =>
+  HasDuplicateEdgesInternal False (Cons k (SLProxy v) t) b
+
+class HasDuplicateEdgesInternal (gate :: Boolean) (graph :: RowList) (b :: Boolean) | gate graph -> b
+
+class HasDuplicateEdges (graph :: # Type) (b :: Boolean) | graph -> b
+
+instance hasDuplicateEdges :: (RowToList graph gl, HasDuplicateEdgesInternal False gl b) => HasDuplicateEdges graph b
+
+class NoDuplicateEdges (graph :: # Type)
+
+instance noDuplicateEdges :: (HasDuplicateEdges graph False) => NoDuplicateEdges graph
+else instance noDuplicateEdgesFail ::
+  ( Fail (Text "This graph has duplicate nodes"), HasDuplicateEdges graph True
+  ) =>
+  NoDuplicateEdges graph
+
+class IncomingNodesInternal (s :: Symbol) (graph :: RowList) (l :: SList) | s graph -> l
+
+instance incomingNodesInternalNil :: IncomingNodesInternal s Nil SNil
+
+instance incomingNodesInternalCons ::
+  ( HasSymbol v s b
+  , SListGate b (SCons k SNil) SNil o
+  , IncomingNodesInternal s t oo
+  , Concat o oo l
+  ) =>
+  IncomingNodesInternal s (Cons k (SLProxy v) t) l
+
+class IncomingNodes (s :: Symbol) (graph :: # Type) (l :: SList) | s graph -> l
+
+instance incomingNodes ::
+  ( RowToList graph gl
+  , IncomingNodesInternal s gl l
+  ) =>
+  IncomingNodes s graph l
+
+instance flipDirectionInternalNil :: FlipDirectionInternal Nil g Nil
+
+instance flipDirectionInternalCons ::
+  ( IncomingNodes k g res
+  , FlipDirectionInternal t g rest
+  ) =>
+  FlipDirectionInternal (Cons k v t) g (Cons k (SLProxy res) rest)
+
+class FlipDirectionInternal (graphA :: RowList) (graph :: # Type) (graphB :: RowList) | graphA graph -> graphB
+
+class FlipDirection (graphA :: # Type) (graphB :: # Type) | graphA -> graphB
+
+instance flipDirection ::
+  ( RowToList graphA glA
+  , FlipDirectionInternal glA graphA glB
+  , ListToRow glB graphB
+  ) =>
+  FlipDirection graphA graphB
--- test/Main.purs
+++ test/Main.purs
@@ -7,7 +7,9 @@ import Prim.Boolean (False, True)
 import Prim.RowList (class RowToList)
 import Record.Extra (SLProxy(..), SNil)
 import Type.Data.Boolean (BProxy(..))
-import Type.Data.Graph (class AllEdges, class Concat, class HasOrphanNodes, class HasSymbol, class IsConnected, class Lookup, class RemoveDuplicates, class Traversal, type (:/))
+import Type.Data.Graph (class AllEdges, class Concat, class Connected, class FlipDirection, class HasDuplicateEdges, class HasDuplicateNodes, class HasOrphanNodes, class HasSymbol, class HasUniqueTerminus, class IncomingNodes, class IsConnected, class Lookup, class NTermini, class NoDuplicateEdges, class NoDuplicateNodes, class NoOrphanedNodes, class RemoveDuplicates, class Traversal, class WithUniqueTerminus, type (:/))
+import Type.Data.Peano (D3, NProxy(..), Z)
+import Type.Data.Row (RProxy(..))
 
 testHasSymbol :: BProxy True
 testHasSymbol =
@@ -167,6 +169,29 @@ testConnected4 =
     IsConnected ( b :: (SLProxy ("c" :/ SNil)), a :: (SLProxy ("q" :/ SNil)) ) b =>
     BProxy b
 
+testConnected5 :: BProxy True
+testConnected5 =
+  BProxy ::
+    forall b.
+    IsConnected
+      ( b :: (SLProxy ("c" :/ "d" :/ SNil))
+      , d :: (SLProxy SNil)
+      , a :: (SLProxy ("b" :/ SNil))
+      )
+      b =>
+    BProxy b
+
+connected :: forall (graph :: # Type). Connected graph => Record graph -> Unit
+connected _ = unit
+
+r =
+  connected
+    { b: SLProxy :: (SLProxy ("c" :/ "d" :/ SNil))
+    , d: SLProxy :: (SLProxy SNil)
+    , a: SLProxy :: (SLProxy ("b" :/ SNil))
+    } ::
+    Unit
+
 testConnected :: BProxy True
 testConnected =
   BProxy ::
@@ -219,6 +244,225 @@ hasOprhanNodes1 =
       )
       b =>
     BProxy b
+
+noOrphanedNodes :: forall (graph :: # Type). NoOrphanedNodes graph => Record graph -> Unit
+noOrphanedNodes _ = unit
+
+z =
+  noOrphanedNodes
+    { a: SLProxy :: SLProxy SNil
+    , r: SLProxy :: SLProxy SNil
+    , p: SLProxy :: SLProxy SNil
+    , q: SLProxy :: SLProxy ("a" :/ "r" :/ SNil)
+    , f: SLProxy :: SLProxy ("r" :/ "p" :/ SNil)
+    } ::
+    Unit
+
+nTermini1 :: NProxy D3
+nTermini1 =
+  NProxy ::
+    forall n.
+    NTermini
+      ( "a" :: SLProxy SNil
+      , "r" :: SLProxy SNil
+      , "p" :: SLProxy SNil
+      , "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "f" :: SLProxy ("r" :/ "p" :/ SNil)
+      )
+      n =>
+    NProxy n
+
+nTermini2 :: NProxy Z
+nTermini2 =
+  NProxy ::
+    forall n.
+    NTermini
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "f" :: SLProxy ("r" :/ "p" :/ SNil)
+      )
+      n =>
+    NProxy n
+
+hasUniqueTerminus1 :: BProxy False
+hasUniqueTerminus1 =
+  BProxy ::
+    forall b.
+    HasUniqueTerminus
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "f" :: SLProxy ("r" :/ "p" :/ SNil)
+      )
+      b =>
+    BProxy b
+
+hasUniqueTerminus2 :: BProxy True
+hasUniqueTerminus2 =
+  BProxy ::
+    forall b.
+    HasUniqueTerminus
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "a" :: SLProxy SNil
+      , "f" :: SLProxy ("r" :/ "p" :/ SNil)
+      )
+      b =>
+    BProxy b
+
+withUniqueTerminus :: forall (graph :: # Type). WithUniqueTerminus graph => Record graph -> Unit
+withUniqueTerminus _ = unit
+
+w =
+  withUniqueTerminus
+    { "q": SLProxy :: SLProxy ("a" :/ "r" :/ SNil)
+    , "a": SLProxy :: SLProxy SNil
+    , "f": SLProxy :: SLProxy ("r" :/ "p" :/ SNil)
+    } ::
+    Unit
+
+hasDuplicateNodes1 :: BProxy False
+hasDuplicateNodes1 =
+  BProxy ::
+    forall b.
+    HasDuplicateNodes
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "a" :: SLProxy SNil
+      , "f" :: SLProxy ("r" :/ "p" :/ SNil)
+      )
+      b =>
+    BProxy b
+
+ndnTest :: forall (graph :: # Type). NoDuplicateNodes graph => Record graph -> Unit
+ndnTest _ = unit
+
+x =
+  ndnTest
+    { q: SLProxy :: SLProxy ("a" :/ "r" :/ SNil)
+    , a: SLProxy :: SLProxy SNil
+    , f: SLProxy :: SLProxy ("r" :/ "p" :/ SNil)
+    } ::
+    Unit
+
+hasDuplicateNodes2 :: BProxy True
+hasDuplicateNodes2 =
+  BProxy ::
+    forall b.
+    HasDuplicateNodes
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "a" :: SLProxy SNil
+      , "f" :: SLProxy ("r" :/ "p" :/ SNil)
+      , "q" :: SLProxy SNil
+      )
+      b =>
+    BProxy b
+
+hasDuplicateEdges1 :: BProxy False
+hasDuplicateEdges1 =
+  BProxy ::
+    forall b.
+    HasDuplicateEdges
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "a" :: SLProxy SNil
+      , "f" :: SLProxy ("r" :/ "p" :/ SNil)
+      )
+      b =>
+    BProxy b
+
+ndeTest :: forall (graph :: # Type). NoDuplicateEdges graph => Record graph -> Unit
+ndeTest _ = unit
+
+o =
+  ndeTest
+    { q: SLProxy :: SLProxy ("a" :/ "r" :/ SNil)
+    , a: SLProxy :: SLProxy SNil
+    , f: SLProxy :: SLProxy ("r" :/ "p" :/ SNil)
+    } ::
+    Unit
+
+hasDuplicateEdges2 :: BProxy False
+hasDuplicateEdges2 =
+  BProxy ::
+    forall b.
+    HasDuplicateEdges
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "a" :: SLProxy SNil
+      , "f" :: SLProxy ("r" :/ "p" :/ SNil)
+      , "q" :: SLProxy SNil
+      )
+      b =>
+    BProxy b
+
+hasDuplicateEdges3 :: BProxy True
+hasDuplicateEdges3 =
+  BProxy ::
+    forall b.
+    HasDuplicateEdges
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "a" :: SLProxy SNil
+      , "f" :: SLProxy ("r" :/ "r" :/ "p" :/ SNil)
+      , "q" :: SLProxy SNil
+      )
+      b =>
+    BProxy b
+
+incomingNodes1 :: SLProxy ("q" :/ SNil)
+incomingNodes1 =
+  SLProxy ::
+    forall sl.
+    IncomingNodes
+      "a"
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "a" :: SLProxy SNil
+      , "f" :: SLProxy ("r" :/ "p" :/ SNil)
+      , "q" :: SLProxy SNil
+      )
+      sl =>
+    SLProxy sl
+
+incomingNodes2 :: SLProxy SNil
+incomingNodes2 =
+  SLProxy ::
+    forall sl.
+    IncomingNodes
+      "q"
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "a" :: SLProxy SNil
+      , "f" :: SLProxy ("r" :/ "p" :/ SNil)
+      , "q" :: SLProxy SNil
+      )
+      sl =>
+    SLProxy sl
+
+incomingNodes3 :: SLProxy ("f" :/ "q" :/ SNil)
+incomingNodes3 =
+  SLProxy ::
+    forall sl.
+    IncomingNodes
+      "a"
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "a" :: SLProxy SNil
+      , "f" :: SLProxy ("a" :/ "p" :/ SNil)
+      )
+      sl =>
+    SLProxy sl
+
+flipDirection ::
+  RProxy
+    ( "q" :: SLProxy SNil
+    , "a" :: SLProxy ("f" :/ "q" :/ SNil)
+    , "f" :: SLProxy SNil
+    , "r" :: SLProxy ("q" :/ SNil)
+    , "p" :: SLProxy ("f" :/ SNil)
+    )
+flipDirection =
+  RProxy ::
+    forall (r :: # Type).
+    FlipDirection
+      ( "q" :: SLProxy ("a" :/ "r" :/ SNil)
+      , "a" :: SLProxy SNil
+      , "f" :: SLProxy ("a" :/ "p" :/ SNil)
+      , "r" :: SLProxy SNil
+      , "p" :: SLProxy SNil
+      )
+      r =>
+    RProxy r
 
 main :: Effect Unit
 main = do
